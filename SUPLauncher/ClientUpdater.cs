8ae6ce3a2152367b85f0e5c98d056958be0d78a8
KingofBeast <kingofbeast@thed3vine.net>
2024-06-26 22:33:31 -0500

    Remove unneeded using statements

--- SUPLauncher/ClientUpdater.cs
+++ SUPLauncher/ClientUpdater.cs
@@ -1,14 +1,8 @@
 ﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
+
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace SUPLauncher
 {
--- SUPLauncher/Overlay.cs
+++ SUPLauncher/Overlay.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Net;
+﻿using System.Net;
 using System.Runtime.InteropServices;
 using System.Text.Json;
 
